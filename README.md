c59ab88ad4829abcb4d1bf8c4f6cf10ac94db334
thegaslessninja <thegaslessninja@gmail.com>
2024-12-21 23:42:02 +0545

    storyhunt v3 sdk

---
+++ README.md
@@ -0,0 +1,9 @@
+# Alpha software
+
+The latest version of the SDK is used in production in the Storyhunt Dex,
+but it is considered Alpha software and may contain bugs or change significantly between patch versions.
+If you have questions about how to use the SDK, please reach out in the `#dev-chat` channel of the Storyhunt Discord.
+
+# Storyhunt V3 SDK
+
+In-depth documentation on this SDK is available at [storyhunt.xyz](https://docs.storyhunt.xyz/).
---
+++ src/constants.ts
@@ -0,0 +1,34 @@
+export const FACTORY_ADDRESS = '0x2344C1448E528dD0e4094c92966A7f68f45aa4e4'
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+
+export function poolInitCodeHash(): string {
+  return POOL_INIT_CODE_HASH
+}
+
+/**
+ * The default factory enabled fee amounts, denominated in hundredths of bips.
+ */
+export enum FeeAmount {
+  LOWEST = 100,
+  LOW_200 = 200,
+  LOW_300 = 300,
+  LOW_400 = 400,
+  LOW = 500,
+  MEDIUM = 3000,
+  HIGH = 10000
+}
+
+/**
+ * The default factory tick spacings by fee amount.
+ */
+export const TICK_SPACINGS: { [amount in FeeAmount]: number } = {
+  [FeeAmount.LOWEST]: 1,
+  [FeeAmount.LOW_200]: 4,
+  [FeeAmount.LOW_300]: 6,
+  [FeeAmount.LOW_400]: 8,
+  [FeeAmount.LOW]: 10,
+  [FeeAmount.MEDIUM]: 60,
+  [FeeAmount.HIGH]: 200
+}
