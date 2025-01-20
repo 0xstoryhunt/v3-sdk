3930d2b14fedcd9e0f2e94761d00e5621f2e9282
thegaslessninja <thegaslessninja@gmail.com>
2025-01-20 22:53:48 +0545

    fix: args pass

--- src/utils/computePoolAddress.ts
+++ src/utils/computePoolAddress.ts
@@ -2,7 +2,7 @@ import { defaultAbiCoder } from '@ethersproject/abi'
 import { getCreate2Address } from '@ethersproject/address'
 import { keccak256 } from '@ethersproject/solidity'
 import { Token } from '@storyhunt/sdk-core'
-import { FeeAmount, poolInitCodeHash } from '../constants'
+import { FeeAmount, POOL_INIT_CODE_HASH, DEPLOYER_ADDRESS } from '../constants'
 
 /**
  * Computes a pool address
@@ -15,16 +15,16 @@ import { FeeAmount, poolInitCodeHash } from '../constants'
  * @returns The pool address
  */
 export function computePoolAddress({
-  deployerAddress,
   tokenA,
   tokenB,
   fee,
+  deployerAddressManualOverride,
   initCodeHashManualOverride
 }: {
-  deployerAddress: string
   tokenA: Token
   tokenB: Token
   fee: FeeAmount
+  deployerAddressManualOverride?: string
   initCodeHashManualOverride?: string
 }): string {
   const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
@@ -32,7 +32,8 @@ export function computePoolAddress({
     ['bytes'],
     [defaultAbiCoder.encode(['address', 'address', 'uint24'], [token0.address, token1.address, fee])]
   )
-  const initCodeHash = initCodeHashManualOverride ?? poolInitCodeHash()
+  const initCodeHash = initCodeHashManualOverride ?? POOL_INIT_CODE_HASH
+  const deployerAddress = deployerAddressManualOverride ?? DEPLOYER_ADDRESS
 
   return getCreate2Address(deployerAddress, salt, initCodeHash)
 }
