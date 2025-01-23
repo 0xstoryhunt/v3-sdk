83026e1d824c8fba75969dc3b5ac52daa43c5284
sam-shariat <samshariat7@gmail.com>
2025-01-23 19:51:43 +0330

    add: alphaHunterV3

--- src/utils/calldata.ts
+++ src/utils/calldata.ts
@@ -20,7 +20,7 @@ export interface MethodParameters {
  * @param bigintIsh
  * @returns The hex encoded calldata
  */
-export function toHex(bigintIsh: BigintIsh) {
+export function toHex(bigintIsh: BigintIsh) : `0x${string}` {
   const bigInt = JSBI.BigInt(bigintIsh)
   let hex = bigInt.toString(16)
   if (hex.length % 2 !== 0) {
