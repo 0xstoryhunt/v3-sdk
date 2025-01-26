e627cd33bd4ddc98e4d0268ee07c270c2bafa4bd
Rahul Shah <rahul.shah9863@gmail.com>
2025-01-26 12:07:38 +0545

    add: addresses from default list

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
-export const FACTORY_ADDRESS = '0x354631ac8fdb2d5d66Ca5809b78BCE9dda1b7973'
-export const DEPLOYER_ADDRESS = '0x0318592f530Ac3C13CD26c197C68b4475e94852d'
+import DEFAULT_ADDRESS_LIST from '@storyhunt/default-list/build/storyhunt-default.constantlist.json';
+import { ChainId } from '@storyhunt/sdk-core';
+export const FACTORY_ADDRESS = DEFAULT_ADDRESS_LIST.constants[ChainId.ODYSSEY].V3_FACTORY_CONTRACT.address;
+export const DEPLOYER_ADDRESS = DEFAULT_ADDRESS_LIST.constants[ChainId.ODYSSEY].V3_POOL_DEPLOYER.address;
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const POOL_INIT_CODE_HASH = '0x5c1ebb91ef1669cb3e664cbf78650858b19d8a4247ca18abab152d5d4f1604db'
+export const POOL_INIT_CODE_HASH = DEFAULT_ADDRESS_LIST.constants[ChainId.ODYSSEY].POOL_INIT_CODE_HASH.address;
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
